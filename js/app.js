0ae80cb249b89551a3769f9198e6999f53f81793
Branden <branden.h.sanders@vanderbilt.edu>
2017-11-20 22:14:50 -0500

    Add default locations and markers

--- js/app.js
+++ js/app.js
@@ -5,13 +5,57 @@ function ViewModel() {
 
     var self = this;
 
-    this.initMap = function() {        
+    this.initMap = function() { 
+        
+        var markers = [];
+        
         // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 26.375721, lng: -80.158954},
-            zoom: 12
+            zoom: 13
         });
-    }    
+
+        var largeInfowindow = new google.maps.InfoWindow();
+        var bounds = new google.maps.LatLngBounds();
+        // The following group uses the location array to create an array of markers on initialize.
+        for (var i = 0; i < locations.length; i++) {
+          // Get the position from the location array.
+          var position = locations[i].location;
+          var title = locations[i].title;
+          // Create a marker per location, and put into markers array.
+          var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+          });
+          // Push the marker to our array of markers.
+          markers.push(marker);
+          // Create an onclick event to open an infowindow at each marker.
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+          });
+          bounds.extend(markers[i].position);
+        }
+        // Extend the boundaries of the map for each marker
+        map.fitBounds(bounds);
+      }
+      // This function populates the infowindow when the marker is clicked. We'll only allow
+      // one infowindow which will open at the marker that is clicked, and populate based
+      // on that markers position.
+      function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
+        }
+      }
 
     this.initMap();    
 }
---
+++ js/locations.js
@@ -0,0 +1,8 @@
+// Default locations to load
+var locations = [
+    {title: 'Boca West Country Club', location: {lat: 26.375721, lng: -80.158172}},
+    {title: 'Boca Beach Club', location: {lat: 26.339996, lng: -80.071057}},
+    {title: 'Cheesecake Factory', location: {lat: 26.367327, lng: -80.129908}},
+    {title: 'Regal Cinemas', location: {lat: 26.369843, lng: -80.201311}},
+    {title: 'Best Buy', location: {lat: 26.371057, lng: -80.200826}},
+    ];
