e99a4d6155ab2d3f207f7eb69838f853bb2cfd70
Branden <branden.h.sanders@vanderbilt.edu>
2017-11-23 13:52:50 -0500

    Add text list and filter functionality

--- js/app.js
+++ js/app.js
@@ -5,24 +5,52 @@ function ViewModel() {
 
     var self = this;
 
-    // Style the default icons
-    var defaultIcon = makeMarkerIcon('8bf08b');
-    
-    // Style the selected icons
+    // Style the default and selected icons
+    var defaultIcon = makeMarkerIcon('8bf08b');   
     var selectedIcon = makeMarkerIcon('001');
+    var markers = [];
+    this.searchQuery = ko.observable("");
+    
+    // This function populates the infowindow when the marker is clicked. We'll only allow
+    // one infowindow which will open at the marker that is clicked, and populate based
+    // on that markers position.
+    this.populateInfoWindow = function(marker, infowindow) {
+      // Check to make sure the infowindow is not already opened on this marker.
+      if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick',function(){
+          infowindow.setMarker = null;
+          marker.setIcon(defaultIcon);            
+        });
+      }
+    }
+    
+    this.selectMarker = function() {
+      // If a marker is selected, deselect it.
+      if (self.currentMarker) {
+        self.currentMarker.setIcon(defaultIcon);
+      }
+      self.currentMarker = this;
+      self.populateInfoWindow(this, self.largeInfowindow);
+      this.setIcon(selectedIcon);      
+    };
 
-    this.initMap = function() { 
-        
-        var markers = [];
-        
+    this.initMap = function() {            
         // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 26.375721, lng: -80.158954},
             zoom: 13
         });
 
-        var largeInfowindow = new google.maps.InfoWindow();
+        // Infowindow object to display info about a marker
+        this.largeInfowindow = new google.maps.InfoWindow();
+
+        // Set the map bounds according to marker lat lngs
         var bounds = new google.maps.LatLngBounds();
+        
         // The following group uses the location array to create an array of markers on initialize.
         for (var i = 0; i < locations.length; i++) {
           // Get the position from the location array.
@@ -40,37 +68,15 @@ function ViewModel() {
           // Push the marker to our array of markers.
           markers.push(marker);
           // Create an onclick event to open an infowindow at each marker.
-          marker.addListener('click', function() {
-            // If a marker is selected, deselect it.
-            if (self.currentMarker) {
-              self.currentMarker.setIcon(defaultIcon);
-            }
-            self.currentMarker = this;
-            populateInfoWindow(this, largeInfowindow);
-            this.setIcon(selectedIcon);
-          });
+          marker.addListener('click', self.selectMarker);
           bounds.extend(markers[i].position);
         }
+        
         // Extend the boundaries of the map for each marker
         map.fitBounds(bounds);
       }
+
       
-      // This function populates the infowindow when the marker is clicked. We'll only allow
-      // one infowindow which will open at the marker that is clicked, and populate based
-      // on that markers position.
-      function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-            marker.setIcon(defaultIcon);            
-          });
-        }
-      }
 
       // This function takes in a COLOR, and then creates a new marker
       // icon of that color. The icon will be 21 px wide by 34 high, have an origin
@@ -87,6 +93,19 @@ function ViewModel() {
       }
 
     this.initMap();    
+
+    this.filteredLocations = ko.computed(function() {
+      var results = [];
+      for (var i = 0; i < markers.length; i++) {
+          if (markers[i].title.toLowerCase().includes(this.searchQuery().toLowerCase())) {
+              results.push(markers[i]);
+              markers[i].setVisible(true);
+          } else {
+              markers[i].setVisible(false);
+          }
+      }
+      return results;
+    }, this);
 }
 
 // If Google maps fails to load, show the following error alert
